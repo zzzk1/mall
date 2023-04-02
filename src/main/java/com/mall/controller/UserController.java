c382b86479fdbf784eae1a22e2f18c695fb068f0
zzzk1 <madzhou1@gmail.com>
2023-04-02 11:33:29 +0800

    swagger

--- src/main/java/com/mall/controller/UserController.java
+++ src/main/java/com/mall/controller/UserController.java
@@ -7,9 +7,12 @@ import com.mall.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import java.util.List;
 
+@Api(tags = "UserController")
 @Controller
 @RestController
 @RequestMapping("/user")
@@ -17,12 +20,17 @@ public class UserController {
     @Autowired
     UserService service;
 
+    @ApiOperation("获取用户分页")
     @GetMapping("/get/pages")
-    public CommonResult<CommonPage<TUser>> getPage(@RequestParam(value = "pageNum", defaultValue = "1") int pageNum,
-                                                   @RequestParam(value = "pageSize", defaultValue = "3") int pageSize) {
+    public CommonResult<CommonPage<TUser>> getPage(@RequestParam(value = "pageNum", defaultValue = "1")
+                                                   @ApiParam("页码") int pageNum,
+                                                   @RequestParam(value = "pageSize", defaultValue = "3")
+                                                   @ApiParam("每页数量") int pageSize) {
         List<TUser> tUsers = service.getPageUser(pageNum, pageSize);
         return CommonResult.success(CommonPage.resultPage(tUsers));
     }
+
+    @ApiOperation("获取用户")
     @GetMapping("/get/{id}")
     public CommonResult<TUser> getUser(@PathVariable int id) {
         TUser user = service.getUser(id);
@@ -33,6 +41,7 @@ public class UserController {
         }
     }
 
+    @ApiOperation("获取用户列表")
     @GetMapping("/get")
     public CommonResult<List<TUser>> getAllUser() {
         List<TUser> userList = service.getAll();
@@ -43,6 +52,7 @@ public class UserController {
         }
     }
 
+    @ApiOperation("添加用户")
     @PostMapping("/add")
     public CommonResult addUser(@RequestBody TUser tUser) {
         int count = service.addUser(tUser);
@@ -53,6 +63,7 @@ public class UserController {
         }
     }
 
+    @ApiOperation("根据id删除用户")
     @DeleteMapping("/delete/{id}")
     public CommonResult deleteUser(@PathVariable int id) {
         int count = service.deleteUser(id);
@@ -62,7 +73,7 @@ public class UserController {
             return CommonResult.failed("操作失败");
         }
     }
-
+    @ApiOperation("根据id更新用户信息")
     @PutMapping("/update/{id}")
     public CommonResult updateUser(@PathVariable int id, @RequestBody TUser tUser) {
         int count = service.updateUser(id, tUser);
